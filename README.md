3d322175006f6fff3efdf59777a900e23cf712d1
Adeyemi Azeem <azeemite@yahoo.com>
2023-03-27 19:39:13 +0100

    Deploying the Model on Flask Api

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Abalone
+### Abalone Age Prediction
+
+### Software And Tools Requirements
+
+1. [Github Account](https://github.com)
+2. [VS code IDE](https://code.visualstudio.com/)
+4. [GitCLI](https://git-scm.com/book/en/v2/Getting-Started-The-Comand-Line)
+
+Create a new environment
+
+---
+
+conda create -p venv python==3.7 -y
--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 import json
 import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask,request,app,jsonify,url_for,render_template, send_from_directory
 import numpy as np
 import pandas as pd
+import os
+
+app=Flask(__name__)
+
+#Load the model
+refmodel=pickle.load(open('rf_model.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=refmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The Age Prediction of the Abalone is {}".format(output))
+
+
+@app.route('/bck.jpg')
+def background():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'bck.jpg')
+
+if __name__=="__main__":
+    app.run(debug=True)
